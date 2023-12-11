5a9186ac2760cf68d448cfe681a8c66ee925be11
Robert <60601627+robertm2301@users.noreply.github.com>
2023-12-11 16:33:09 +0000

    Add items to array list screen, view all items with option to delete screen

--- src/StockHome.java
+++ src/StockHome.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class StockHome extends JFrame {
+
+    private ArrayList<Item> itemList;
 
     public StockHome() {
         setTitle("Stock Home");
@@ -11,17 +15,30 @@ public class StockHome extends JFrame {
         setSize(700, 500);
         setLocationRelativeTo(null);
 
-        // Create ImageIcons for the button images
+        itemList = new ArrayList<>();
+
         ImageIcon button1Icon = new ImageIcon("/Users/robert/Library/CloudStorage/OneDrive-TechnologicalUniversityDublin/BusinessComputingTU914/Year 3/Business Modelling/CA3_BusinessModelling/CA3_BusinessModelling/src/plus.png");
         ImageIcon button2Icon = new ImageIcon("/Users/robert/Library/CloudStorage/OneDrive-TechnologicalUniversityDublin/BusinessComputingTU914/Year 3/Business Modelling/CA3_BusinessModelling/CA3_BusinessModelling/src/searchImg.png");
 
         JButton button1 = new JButton();
         button1.setIcon(new ImageIcon(button1Icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
         button1.setText("Add Items ");
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openAddItemScreen();
+            }
+        });
 
         JButton button2 = new JButton();
         button2.setIcon(new ImageIcon(button2Icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
         button2.setText("View All Items");
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                viewAllItems();
+            }
+        });
 
         setLayout(new BorderLayout());
 
@@ -41,14 +58,110 @@ public class StockHome extends JFrame {
         homeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Open the HomePage
                 new HomePage().setVisible(true);
-                // Close the current StockHome frame
                 dispose();
             }
         });
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void openAddItemScreen() {
+        JFrame addItemFrame = new JFrame("Add Item");
+        addItemFrame.setSize(400, 300);
+        addItemFrame.setLocationRelativeTo(null);
+
+        JPanel addItemPanel = new JPanel(new GridLayout(6, 2, 10, 10));
+
+        JLabel nameLabel = new JLabel("Item Name:");
+        JTextField nameField = new JTextField();
+
+        JLabel typeLabel = new JLabel("Item Type:");
+        String[] itemTypes = {"Luxury", "Essential", "Gift"};
+        JComboBox<String> typeComboBox = new JComboBox<>(itemTypes);
+
+        JLabel expirationLabel = new JLabel("Expiration Date:");
+        JTextField expirationField = new JTextField();
+
+        JLabel priceLabel = new JLabel("Price:");
+        JTextField priceField = new JTextField();
+
+        JLabel emptyLabel = new JLabel();
+
+        JButton addItemButton = new JButton("Add Item");
+        addItemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = nameField.getText();
+                String type = (String) typeComboBox.getSelectedItem();
+                String expirationDate = expirationField.getText();
+                double price = Double.parseDouble(priceField.getText());
+
+                Item newItem = new Item(name, type, expirationDate, price);
+                itemList.add(newItem);
+
+                addItemFrame.dispose();
+            }
+        });
+
+        addItemPanel.add(nameLabel);
+        addItemPanel.add(nameField);
+        addItemPanel.add(typeLabel);
+        addItemPanel.add(typeComboBox);
+        addItemPanel.add(expirationLabel);
+        addItemPanel.add(expirationField);
+        addItemPanel.add(priceLabel);
+        addItemPanel.add(priceField);
+        addItemPanel.add(emptyLabel);
+        addItemPanel.add(addItemButton);
+
+        addItemFrame.add(addItemPanel);
+        addItemFrame.setVisible(true);
+    }
+
+    private void viewAllItems() {
+        JFrame viewAllItemsFrame = new JFrame("View All Items");
+        viewAllItemsFrame.setSize(800, 500);
+        viewAllItemsFrame.setLocationRelativeTo(null);
+
+        DefaultTableModel tableModel = new DefaultTableModel(new Object[]{"Name", "Type", "Expiry", "Price"}, 0);
+
+        for (Item item : itemList) {
+            tableModel.addRow(new Object[]{item.getName(), item.getType(), item.getExpirationDate(), item.getPrice()});
+        }
+
+        JTable table = new JTable(tableModel);
+
+        JButton deleteButton = new JButton("Delete");
+        JButton returnButton = new JButton("Return");
+
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selectedRow = table.getSelectedRow();
+                if (selectedRow != -1) {
+                    itemList.remove(selectedRow);
+                    tableModel.removeRow(selectedRow);
+                }
+            }
+        });
+
+        returnButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                viewAllItemsFrame.dispose();
+            }
+        });
+
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+        buttonPanel.add(deleteButton);
+        buttonPanel.add(returnButton);
+
+        viewAllItemsFrame.setLayout(new BorderLayout());
+        viewAllItemsFrame.add(new JScrollPane(table), BorderLayout.CENTER);
+        viewAllItemsFrame.add(buttonPanel, BorderLayout.SOUTH);
+
+        viewAllItemsFrame.setVisible(true);
     }
 
     public static void main(String[] args) {
@@ -58,5 +171,35 @@ public class StockHome extends JFrame {
                 new StockHome().setVisible(true);
             }
         });
+    }
+
+    private static class Item {
+        private String name;
+        private String type;
+        private String expirationDate;
+        private double price;
+
+        public Item(String name, String type, String expirationDate, double price) {
+            this.name = name;
+            this.type = type;
+            this.expirationDate = expirationDate;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getExpirationDate() {
+            return expirationDate;
+        }
+
+        public double getPrice() {
+            return price;
+        }
     }
 }
